f0a0b171ce213e2e024811b08d1db03f099d9338
Mesud melaku <149295529+ellay21@users.noreply.github.com>
2025-05-19 06:28:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Location: https://petstore.swagger.io/?url=' . urlencode('https://yourdomain.com/swagger.yaml'));
+// Check if requesting the YAML file
+if ($_SERVER['REQUEST_URI'] === '/swagger.yaml') {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/x-yaml");
+    readfile(__DIR__.'/swagger.yaml');
+    exit();
+}
+
+// For all other requests, redirect to Swagger UI
+header('Location: https://petstore.swagger.io/?url='.urlencode('https://'.$_SERVER['HTTP_HOST'].'/swagger.yaml'));
 exit();
